92d90e87068b88ba53e46e0ccd9d073a675af6e3
hyperion2144 <2840926925@qq.com>
2023-09-02 09:58:10 +0800

    new file:   402.remove-k-digits.go

---
+++ 402.remove-k-digits.go
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=402 lang=golang
+ * @lcpr version=21913
+ *
+ * [402] 移掉 K 位数字
+ *
+ * https://leetcode.cn/problems/remove-k-digits/description/
+ *
+ * algorithms
+ * Medium (31.73%)
+ * Likes:    979
+ * Dislikes: 0
+ * Total Accepted:    141.2K
+ * Total Submissions: 444.9K
+ * Testcase Example:  '"1432219"\n3'
+ *
+ * 给你一个以字符串表示的非负整数 num 和一个整数 k ，移除这个数中的 k 位数字，使得剩下的数字最小。请你以字符串形式返回这个最小的数字。
+ *
+ *
+ * 示例 1 ：
+ *
+ * 输入：num = "1432219", k = 3
+ * 输出："1219"
+ * 解释：移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219 。
+ *
+ *
+ * 示例 2 ：
+ *
+ * 输入：num = "10200", k = 1
+ * 输出："200"
+ * 解释：移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+ *
+ *
+ * 示例 3 ：
+ *
+ * 输入：num = "10", k = 2
+ * 输出："0"
+ * 解释：从原数字移除所有的数字，剩余为空就是 0 。
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1 <= k <= num.length <= 10^5
+ * num 仅由若干位数字（0 - 9）组成
+ * 除了 0 本身之外，num 不含任何前导零
+ *
+ *
+ */
+
+// @lc code=start
+func removeKdigits(num string, k int) string {
+	stack := []byte{}
+	for i := range num {
+		digit := num[i]
+		for k > 0 && len(stack) > 0 && digit < stack[len(stack)-1] {
+			stack = stack[:len(stack)-1]
+			k--
+		}
+		stack = append(stack, digit)
+	}
+	stack = stack[:len(stack)-k]
+	ans := strings.TrimLeft(string(stack), "0")
+	if ans == "" {
+		ans = "0"
+	}
+	return ans
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// "1432219"\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "10200"\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "10"\n2\n
+// @lcpr case=end
+
+*/
+
--- 5.longest-palindromic-substring.go
+++ 5.longest-palindromic-substring.go
@@ -46,11 +46,37 @@
 
 // @lc code=start
 func longestPalindrome(s string) string {
-	var ans string
-	for i := 0; i < len(s); i++ {
-
+	if len(s) < 2 {
+		return s
 	}
-	return ans
+
+	var start, end int
+	for i := 0; i < len(s); i++ {
+		left1, right1 := expandAroundCenter(s, i, i)
+		left2, right2 := expandAroundCenter(s, i, i+1)
+		if right1-left1 > end-start {
+			start, end = left1, right1
+		}
+		if right2-left2 > end-start {
+			start, end = left2, right2
+		}
+	}
+	return s[start : end+1]
+}
+
+func expandAroundCenter(s string, left, right int) (int, int) {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return left + 1, right - 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 // @lc code=end
